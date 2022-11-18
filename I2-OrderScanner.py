ba2fe4d0889ad0cf18d8e136ef7a2ada1e43ce98
GeWei <grey@pku.edu.cn>
2022-11-18 10:48:58 +0800

    finish queryOrder

--- I2-OrderScanner.py
+++ I2-OrderScanner.py
@@ -1,139 +1,87 @@
 import time
-import configparser
+import csv
 import dbf
 
-class XHPT():
-    """docstring for XHPT."""
-    def __init__(self, runDate, moniterDir):
-        self._moniterDir=moniterDir
-        self._runDate=runDate
-        self._readConfig()
-        self._orderList=[]
-        self._cancelList=[]
-        self._pendingList=[]
+class i2OrderScanner:
+    def __init__(self, moniterDir):
+        self._moniterDir = moniterDir
+        self._runDate = time.strftime("%Y%m%d")
 
-    def _readConfig(self):
-        config = configparser.ConfigParser()
-        config.read('config.ini')
-        self._zcdybh=config['DEFAULT']['ZCDYBH']
-        self._zhbh=config['DEFAULT']['ZHBH']
-        self._counter=int(config['DEFAULT']['COUNTER'])
-
-    def _writeConfig(self):
-        config = configparser.ConfigParser()
-        config['DEFAULT']={'ZCDYBH':self._zcdybh, 'ZHBH':self._zhbh, 'COUNTER':str(self._counter)}
-        with open('config.ini', 'w') as configfile:
-            config.write(configfile)
-
-    def _writeDBF(self, flag):
-        if flag=='WT':
-            table=dbf.Table(filename=f'{self._moniterDir}\\XHPT_WT{self._runDate}.dbf', codepage='cp936')
-            table.open(mode=dbf.READ_WRITE)
-            for _order in self._orderList:
-                table.append(_order)
-        elif flag=='CD':
-            table=dbf.Table(filename=f'{self._moniterDir}\\XHPT_CD{self._runDate}.dbf', codepage='cp936')
-            table.open(mode=dbf.READ_WRITE)
-            for _cancel in self._cancelList:
-                table.append(_cancel)
-        
+    def _writeDBF(self, file_name, record_list):
+        table = dbf.Table(filename=file_name, codepage="cp936")
+        table.open(mode=dbf.READ_WRITE)
+        for record in record_list:
+            table.append(record)
         table.close()
 
-    def _readDBF(self, flag):
-        if flag=='WTCX':
-            table=dbf.Table(filename=f'{self._moniterDir}\\XHPT_WTCX{self._runDate}.dbf', codepage='cp936')
-            table.open(mode=dbf.READ_ONLY)
-            self._pendingList=[record for record in table]
-        elif flag=='CJCX':
-            table=dbf.Table(filename=f'{self._moniterDir}\\XHPT_CJCX{self._runDate}.dbf', codepage='cp936')
-            table.open(mode=dbf.READ_ONLY)
-            for record in table:
-                print(record)
+    def _readDBF(self, file_name):
+        table = dbf.Table(filename=file_name, codepage="cp936")
+        table.open(mode=dbf.READ_ONLY)
+        record_list = [record for record in table]
+        table.close()
+        return record_list
 
-    def order(self, orderNumber, CPBH, JYSC, ZQDM, WTFX, WTJGLX, WTJG, WTSL):
-        """
-        orderNumber: 委托笔数
-        CPBH: 产品编号
-        JYSC: 交易市场
-        ZQDM: 证券代码
-        WTFX: 委托方向, 1买入; 2卖出
-        WTJGLX: 委托价格类型
-        WTJG: 委托价格
-        WTSL: 委托数量
-        """
-        gddm=None
-        if JYSC=='1': # 上交所
-            gddm='E000000509'
-        elif JYSC=='2': # 深交所
-            gddm='0001520507'
-        elif JYSC=='j': # 股转系统
-            gddm='4520000920'
-        elif JYSC=='r': # 北交所
-            gddm='4520000920'
+    @staticmethod
+    def readCSV(filename):
+        with open(filename, "r", encoding="utf8") as file:
+            reader = csv.DictReader(file)
+            return list(reader)
 
-        self._orderList=[]
-        for i in range(orderNumber):
-            self._counter+=1
-            self._orderList.append({
-                "CPBH": CPBH, 
-                "ZCDYBH": self._zcdybh, # 单元编号
-                "ZHBH": self._zhbh, # 组合编号
-                "GDDM": gddm, 
-                "JYSC": JYSC, 
-                "ZQDM": ZQDM, 
-                "WTFX": WTFX, 
-                "WTJGLX": WTJGLX, 
-                "WTJG": WTJG, 
-                "WTSL": WTSL, 
-                "WBZDYXH": self._counter# 自定义编号                
-            })
-        print(f'下单:{len(self._orderList)}条')
-        self._writeDBF(flag='WT')
-        self._writeConfig()
-        
-    def cancel(self, wtxhs):
-        '''
-        WTXH: 委托序号，用于撤单
-        '''
-        self._cancelList=[]
-        for xh in wtxhs:
-            self._cancelList.append({
-                'WTXH':xh,
-            })
-        self._writeDBF(flag='CD')
-        
+    @staticmethod
+    def writeCSV(filename, data_list):
+        if len(data_list) == 0:
+            print("data list is empty!")
+            return
+        colNames = data_list[0].keys()
+        with open(filename, "w", encoding="utf8", newline="") as file:
+            csv_writer = csv.DictWriter(file, fieldnames=colNames)
+            csv_writer.writeheader()
+            csv_writer.writerows(data_list)
+
+
     def queryOrder(self):
-        self._readDBF(flag='WTCX')
-
+        filename=f'{self._moniterDir}\\XHPT_WTCX{self._runDate}.dbf'
+        record_list = self._readDBF(filename)
+        data_list=[
+            {
+                '委托日期时间':f'{record.WTRQ}{record.WTSJ}',
+                '委托序号':record.WTXH,
+                '外部自定义序号':record.WBZDYXH,
+                '产品编号':record.CPBH.strip(),
+                '资产单元编号':record.ZCDYBH.strip(),
+                '组合编号':record.ZHBH.strip(),
+                '股东代码':record.GDDM.strip(),
+                '证券代码': record.ZQDM.strip(),
+                '委托价格类型':record.WTJGLX,
+                'Direction':record.WTFX.strip(),
+                'Price':record.WTJG,
+                'Volume':record.WTSL,
+                '预买冻结金额':record.YMDJJE,
+                '预卖收入金额':record.YMSRJE,
+                'OrderStatus':record.WTZT,
+                '委托撤成数量':record.WTCCSL,
+                '废单原因':record.FDYY.strip(),
+                '交易所申报编号':record.JYSSBBH.strip(),
+                # '处理标志':record.CLBZ.strip(),
+                # '备用字段':record.BYZD.strip(),
+                '委托金额':record.WTJE,
+                # '特殊标志':record.TSBS.strip(),
+            }
+            for record in record_list
+        ]
+        i2OrderScanner.writeCSV('output/queryOrder.csv', data_list)
+    
     def queryTrade(self):
-        self._readDBF(flag='CJCX')
-
-    def auto_cancel(self, delay=10):
-        time.sleep(delay)
-        self.queryOrder()
-        # 只有状态为已报(4),部成(6)的委托才能撤单
-        wtxhs=[record.WTXH for record in self._pendingList if (record.WTZT=='4' or record.WTZT=='6')]
-        # 清空队列
-        self._pendingList=[]
-        # print(wtxhs)
-        if wtxhs:
-            # 撤单
-            self.cancel(wtxhs)
-            print(f'撤单:{len(wtxhs)}条')
+        filename=f'{self._moniterDir}\\XHPT_CJCX{self._runDate}.dbf'
+        record_list = self._readDBF(filename)
+        for record in record_list:
+            pass
+        print(record)
 
 
 if __name__ == "__main__":
-    obj=XHPT(runDate='20220712', moniterDir='D:\\MyWork\\MaidanDir')
-    # 下单10手, 深交所000001限价
-    obj.order(
-        orderNumber=10, 
-        CPBH='00010009', 
-        JYSC='2', 
-        ZQDM='000001', 
-        WTFX='1', 
-        WTJGLX='0', 
-        WTJG=14.17, 
-        WTSL=100
-        )
-    # 下委托10s后，未成交撤单
-    obj.auto_cancel(delay=10)
+    obj=i2OrderScanner(moniterDir=r'D:\MyWork\MaidanDir')
+
+    # # query Order, Trade
+    obj.queryOrder()
+    # obj.queryTrade()
